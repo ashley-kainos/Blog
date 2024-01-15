c039028fc639a88ba8f143e69c114587c85c7164
AshleyRutlandBlog <156668748+AshleyRutlandBlog@users.noreply.github.com>
2024-01-15 11:40:03 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "starter-kit" {
+  name     = "rg-starter-kit"
+  location = "uksouth"
+}
+
+resource "azurerm_storage_account" "starter-kit" {
+  name                     = "startkitstorageaccount"
+  resource_group_name      = azurerm_resource_group.starter-kit.name
+  location                 = azurerm_resource_group.starter-kit.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "starter-kit"
+  }
+}
+resource "azurerm_storage_container" "tfstate" {
+  name                 = "tfstate"
+  storage_account_name = azurerm_storage_account.starter-kit.name
+}
